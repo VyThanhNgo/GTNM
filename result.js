29c9086596ee8b04fef105d08b3b9228029efbb8
VyThanhNgo <23130393@st.hcmuaf.edu.vn>
2025-01-19 19:12:08 +0700

    Delete result.js

--- result.js
+++
@@ -1,5 +0,0 @@
-function toggleDetails(memberElement) {
-    const details = memberElement.querySelector('.member-details');
-    // Toggle visibility of member details
-    details.style.display = (details.style.display === 'block') ? 'none' : 'block';
-}
