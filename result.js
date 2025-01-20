417a8051a210a687a1a13ba37eafe9b4b0ab88d7
VyThanhNgo <23130393@st.hcmuaf.edu.vn>
2025-01-20 12:36:24 +0700

    Update result.js

--- result.js
+++ result.js
@@ -3,3 +3,11 @@ function toggleDetails(memberElement) {
     // Toggle visibility of member details
     details.style.display = (details.style.display === 'block') ? 'none' : 'block';
 }
+document.addEventListener('DOMContentLoaded', () => {
+    const slider = document.getElementById('selfRating');
+    const ratingValue = document.getElementById('ratingValue');
+
+    slider.addEventListener('input', () => {
+        ratingValue.textContent = `${slider.value}%`;
+    });
+});
